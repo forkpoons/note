0073c79c2178e1c3a604b25b5fcca7a1c97fedba
forkpoons <forkpoons@gmail.com>
2018-10-29 14:24:19 +0700

    first

--- src/App.js
+++ src/App.js
@@ -2,24 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const notebooks = [
+    {
+        name: 'notebook1'
+    },
+    {
+        name: 'notebook2'
+    },
+    {
+        name: 'notebook3'
+    }
+];
+
+class NotebookList extends Component{
+    render() {
+        return (
+            notebooks.map(function (item) {
+                return(
+                    <li>{item.name}</li>
+                );
+            })
+      )
+    }
+}
+
+class Menu extends Component{
+    render() {
+        return (
+            <div className="menu">
+                <div className="logo">
+                    <span className="logo-img"> </span> Note
+                </div>
+                <div className="new-note">
+                    <div className="new-note-img"><span className="new-note-img1"></span><span className="new-note-img1"></span> </div>
+                    <div className="new-note-text"> New note</div>
+
+                </div>
+                <ul className="menu-list">
+                    <li>All notes</li>
+                    <li>Notebook</li>
+                    <ul>
+                        <NotebookList/>
+                    </ul>
+                </ul>
+            </div>
+        )
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu/>
       </div>
     );
   }
